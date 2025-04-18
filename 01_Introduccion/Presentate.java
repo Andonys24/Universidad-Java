43bb0ef0cdc2383e83d71b32d9ff81f67f9c0062
Andonys24 <andonihernandez1990@gmail.com>
2025-04-18 01:31:18 -0600

    01 Introduccion a Java
    - Primer Hola Mundo
    
    * Retos:
    - Presentate
    - Saludo

---
+++ 01_Introduccion/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Andoni Hernandez");
+        System.out.println("Edad: 19 anios");
+        System.out.println("Pais: Honduras");
+    }
+}
