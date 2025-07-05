f4047cf375adb2f6f6e5d8adbdf23862c974be5a
Andonys24 <andonihernandez1990@gmail.com>
2025-07-04 22:56:52 -0600

    16 App Zona Fit con Swing - Creacion de la tabla para listar los clientes - Creacion de los cabeceros de la tabla - Listando los clinetes y mostrandolos en la tabla

--- 16_GUI_Swing/zonaFitSpring/src/main/java/gm/zona_fit/gui/ZonaFitForma.java
+++ 16_GUI_Swing/zonaFitSpring/src/main/java/gm/zona_fit/gui/ZonaFitForma.java
@@ -6,11 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 @Component
 public class ZonaFitForma extends JFrame {
     private JPanel panelPrincipal;
+    private JPanel panelBotones;
+    private JPanel panelTabla;
+    private JPanel panelDatosCliente;
+    private JTable clientesTabla;
     IClienteService clienteService;
+    private DefaultTableModel tablaModeloClientes;
 
 
     @Autowired
@@ -24,5 +30,35 @@ public class ZonaFitForma extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(900, 700); // Tamano ventana
         setLocationRelativeTo(null); // Centrar ventana
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        this.tablaModeloClientes = new DefaultTableModel(0, 4);
+        String[] cabeceros = {"ID", "Nombre", "Apellido", "Membresia"};
+        this.tablaModeloClientes.setColumnIdentifiers(cabeceros);
+        this.clientesTabla = new JTable(tablaModeloClientes) {
+            // Evitar q el usuario pueda modificar la tabla
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+
+        // Cargar listado de clientes
+        listarClientes();
+    }
+
+    private void listarClientes() {
+        this.tablaModeloClientes.setRowCount(0); // Iniciar conteo en 0
+        var clientes = this.clienteService.listatClientes();
+
+        clientes.forEach(cliente -> {
+            Object[] renglonCliente = {
+                    cliente.getId(), cliente.getNombre(),
+                    cliente.getApellido(), cliente.getMembresia()
+            };
+            this.tablaModeloClientes.addRow(renglonCliente);
+        });
     }
 }
