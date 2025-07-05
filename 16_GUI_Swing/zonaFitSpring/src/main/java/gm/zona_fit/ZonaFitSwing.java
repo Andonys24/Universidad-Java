78ac84fc6c68416904c7abad77d439a2342e3a6b
Andonys24 <andonihernandez1990@gmail.com>
2025-07-04 22:08:03 -0600

    16 App Zona Fit con Swing * Implementacion de modo oscuro     - Agregando las dependencias de com.formdev en el pom.xml     - Configurar el modo oscuro antes de instanciar la fabrica de SpringBoot

--- 16_GUI_Swing/zonaFitSpring/src/main/java/gm/zona_fit/ZonaFitSwing.java
+++ 16_GUI_Swing/zonaFitSpring/src/main/java/gm/zona_fit/ZonaFitSwing.java
@@ -1,5 +1,6 @@
 package gm.zona_fit;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
 import gm.zona_fit.gui.ZonaFitForma;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,8 @@ import javax.swing.*;
 public class ZonaFitSwing {
 
     public static void main(String[] args) {
+        // Configurar Modo Oscuro
+        FlatDarculaLaf.setup();
         // Instanciar la fabrica de SpringBoot
         ConfigurableApplicationContext contextoSpring = new SpringApplicationBuilder(
                 ZonaFitSwing.class
